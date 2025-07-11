189cc309a32e15aa8d4545aea0ddf9045b6de5dd
DARKWOLF60 <darkw6403@gmail.com>
2025-07-11 17:45:58 +0200

    v0.0.2

--- To-Do_app.py
+++ To-Do_app.py
@@ -1,1 +1,28 @@
 #TO-DO app in terminal 
+# Make it print "Welcome to your to-do app" *
+#Let the user create a task 
+#Save it to a list 
+#Show the list 
+
+#Add the time for the task
+#Add the day for the task 
+
+
+tasks = []
+
+print('Welcome to your to-do app \n') 
+
+task = input("Insert the task: ")
+time = input("Insert the time: ")
+date = input("insert the date: ")
+
+task_info = {
+    "task": task,
+    "time": time,
+    "date": date
+}
+tasks.append(task_info)
+
+print("\nYour tasks:" )
+for i in tasks:
+    print(i)
